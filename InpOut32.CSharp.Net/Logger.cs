70f6c7d7a72a1c3a15eeb701ef7f1144954b8ff9
smolsky <smolskye@mail.ru>
2013-12-06 11:52:49 +0400

    Logger. Output cosmetcis

--- InpOut32.CSharp.Net/Logger.cs
+++ InpOut32.CSharp.Net/Logger.cs
@@ -11,10 +11,12 @@ namespace InpOut32.CSharp.Net
     public void Output(int port, uint val)
     {
       var date = DateTime.Now;
+      val = val ^ 0x80;
       var strVal = Convert.ToString(val, 2);
       while (strVal.Length < 32)
         strVal = "0" + strVal;
-      var line = String.Format("{0}> Port: '{1}' Value: {2} {3}", date.ToString("dd MMM HH:mm:ss"), port, strVal, Environment.NewLine );
+      strVal = strVal.Substring(24, 5);
+      var line = String.Format("{0}> Port: '{1}' Value: {2} {3}", date.ToString("dd.MM HH:mm:ss"), port, strVal, Environment.NewLine );
       File.AppendAllText(FilePath, line);      
       
     }
